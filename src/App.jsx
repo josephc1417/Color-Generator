1b9497009b858be5f25f736b74a98c82abef4d1a
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-19 01:36:05 -0400

    First commit update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-impor
+import { Form } from "./components/Form";
+import { SingleColor } from "./components/SingleColor";
+
+
 
 
 const App = () => {
   return(
-    <h2>Color Generator Starter</h2>
-    <Form />
-    <SingleColor />
-
+    <div>
+      <h2>Color Generator Starter</h2>
+      <Form />
+      <SingleColor />
+    </div>
   ) 
 };
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 
-export const  = () => {
+export const Form = () => {
   return (
-    <div></div>
+    <div>Form</div>
   )
 }
