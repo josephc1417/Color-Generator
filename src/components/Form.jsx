09d76022c031307146df510fd305d94e96dc3bd8
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-19 02:02:34 -0400

    Second Commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,29 @@
+import {useState} from 'react'
+
+
 
 export const Form = () => {
+    const [color, setColor] = useState("#e66465")
+
+    const handleSubmit = () => {
+        
+}
   return (
-    <div>Form</div>
+    <div>
+        <h4>Form</h4>
+        <Form className='' onSubmit={handleSubmit}>
+            <label htmlFor='color'>Color</label>
+            <input type='color' name='color' id='head'
+            value={color} 
+            onChange={(e) => setColor(e.target.value)}/>
+            
+            <label htmlFor='text'>HEX</label>
+            <input type='text' name='text' id='text'
+            value={color} 
+            onChange={(e) => setColor(e.target.value)}/>
+            
+        </Form>
+    
+    </div>
   )
 }
